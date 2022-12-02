047698f079f37571a237a7646370bc414317860a
zacki744 <78082188+zacki744@users.noreply.github.com>
2022-12-02 10:14:29 +0100

    Update lib_io.s

--- labb_3/labb 3/lib_io.s
+++ labb_3/labb 3/lib_io.s
@@ -1,5 +1,47 @@
 
 .data
-    buf .quad 0
-    index .quad 0
-.global inImage, outImage
+    buf     .quad 0
+    index   .quad 0
+//input
+.global inImage, getInt, getText, getChar, getInPos, setInPos
+
+//output
+.global outImage, putInt, putText, putChar, getOutPos, getOutPos
+
+//input dec
+inImage:
+    ret
+
+getInt:
+    ret
+
+getText:
+    ret
+
+getChar:
+    ret
+
+getInPos:
+    ret
+    
+setInPos:
+    ret
+
+//output dec
+outImage:
+    ret
+    
+putInt:
+    ret
+    
+putText:
+    ret
+    
+putChar:
+    ret
+    
+getOutPos:
+    ret
+
+getOutPos:
+    ret
